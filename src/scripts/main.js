5779d257c59cbc9b2effa79080889136f255bda5
Niklas <p3er.guessf3ldt@gmail.com>
2020-05-24 01:00:59 +0200

    Everything solved and score added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,41 +1,57 @@
 let gameIsRunning = false;
 const textToType = ["Python", "Hacking", "JavaScript", "TypeScript"];
 let index = 0;
+let score = 0;
+let youCanType = false;
+let timeleft = 10;
+
+const updateScore = (score) => {
+  something.textContent = score;
+};
 
 const showText = (index) => {
   showTextField.textContent = `Word to type now: ${textToType[index]}`;
 };
 
 const checkIfMatch = () => {
-  if (inputField.value == textToType[index]) {
-    index += 1;
-    showText(index);
-    inputField.value = "";
-    something.textContent = "";
-  } else if (inputField.value != textToType[index]) {
-    inputField.value = "";
-    something.textContent = "Please type again!";
+  if (youCanType) {
+    if (inputField.value == textToType[index]) {
+      index += 1;
+      score += 1;
+      showText(index);
+      inputField.value = "";
+      something.textContent = "";
+      updateScore(score);
+    } else if (inputField.value != textToType[index]) {
+      score -= 1;
+      inputField.value = "";
+      something.textContent = "Please type again!";
+      updateScore(score);
+    }
+  } else {
+    inputField.value = "You cant type anymore";
   }
 };
 
-const startCountdown = () => {
-  let timeleft = 10;
+const startCountdown = (timeleft) => {
   let downloadTimer = setInterval(function () {
     countdownField.textContent = timeleft;
     if (timeleft <= 0) {
       clearInterval(downloadTimer);
+      countdownField.textContent =
+        "Your time is over, you cant type anymore!";
+      youCanType = false;
     }
 
     timeleft -= 1;
   }, 1000);
 };
 
-showText(index);
-startCountdown();
-
-// startGameBtn.addEventListener("click", () => {
-//   checkIfMatch();
-// });
+startGameBtn.addEventListener("click", () => {
+  startCountdown(timeleft);
+  showText(index);
+  youCanType = true;
+});
 
 inputField.addEventListener("keyup", (e) => {
   if (e.key == "Enter") {
