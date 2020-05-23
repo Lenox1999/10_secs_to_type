deb9b25bf4666d9c1f46f5c375b32b829f76698c
Niklas <p3er.guessf3ldt@gmail.com>
2020-05-24 00:44:40 +0200

    Revert "connected every element on the screen to work the way it should work... lol"
    This reverts commit a63757bfe00a1c55d640fcff73555c874857a0cc.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,52 +1,41 @@
 let gameIsRunning = false;
 const textToType = ["Python", "Hacking", "JavaScript", "TypeScript"];
 let index = 0;
-let timeleft = 10;
-let youCanType = false;
 
 const showText = (index) => {
   showTextField.textContent = `Word to type now: ${textToType[index]}`;
 };
 
 const checkIfMatch = () => {
-  // if (timeleft <= 0) {
-  //   youCanType = false;
-  // }
-  if (youCanType) {
-    if (inputField.value == textToType[index]) {
-      index += 1;
-      showText(index);
-      inputField.value = "";
-      something.textContent = "";
-    } else if (inputField.value != textToType[index]) {
-      inputField.value = "";
-      something.textContent = "Please type again!";
-    }
-  } else if (!youCanType) {
-    something.textContent = "You cant type anymore!";
-    inputField.value = "You cant type anymore";
+  if (inputField.value == textToType[index]) {
+    index += 1;
+    showText(index);
+    inputField.value = "";
+    something.textContent = "";
+  } else if (inputField.value != textToType[index]) {
+    inputField.value = "";
+    something.textContent = "Please type again!";
   }
 };
 
-const startCountdown = (timeleft) => {
+const startCountdown = () => {
+  let timeleft = 10;
   let downloadTimer = setInterval(function () {
     countdownField.textContent = timeleft;
     if (timeleft <= 0) {
       clearInterval(downloadTimer);
-      countdownField.textContent = "Your time is over!";
-      showTextField.textContent = "Time is over, man!";
-      youCanType = false;
     }
 
     timeleft -= 1;
   }, 1000);
 };
 
-startGameBtn.addEventListener("click", () => {
-  startCountdown(timeleft);
-  youCanType = true;
-  showText(index);
-});
+showText(index);
+startCountdown();
+
+// startGameBtn.addEventListener("click", () => {
+//   checkIfMatch();
+// });
 
 inputField.addEventListener("keyup", (e) => {
   if (e.key == "Enter") {
