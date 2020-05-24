1e7344babacc9cae9fb613c883a4db51659c6297
Niklas <p3er.guessf3ldt@gmail.com>
2020-05-24 12:29:51 +0200

    Changed everything to TypeScript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,60 +1,55 @@
-let gameIsRunning = false;
-const textToType = ["Python", "Hacking", "JavaScript", "TypeScript"];
-let index = 0;
-let score = 0;
-let youCanType = false;
-let timeleft = 10;
-
-const updateScore = (score) => {
-  something.textContent = score;
+// let gameIsRunning = false;
+var textToType = ["Python", "Hacking", "JavaScript", "TypeScript"];
+var index = 0;
+var score = 0;
+var youCanType = false;
+var timeleft = 10;
+var updateScore = function (score) {
+    something.textContent = score;
 };
-
-const showText = (index) => {
-  showTextField.textContent = `Word to type now: ${textToType[index]}`;
+var showText = function (index) {
+    showTextField.textContent = "Word to type now: " + textToType[index];
 };
-
-const checkIfMatch = () => {
-  if (youCanType) {
-    if (inputField.value == textToType[index]) {
-      index += 1;
-      score += 1;
-      showText(index);
-      inputField.value = "";
-      something.textContent = "";
-      updateScore(score);
-    } else if (inputField.value != textToType[index]) {
-      score -= 1;
-      inputField.value = "";
-      something.textContent = "Please type again!";
-      updateScore(score);
+var checkIfMatch = function () {
+    if (youCanType) {
+        if (inputField.value == textToType[index]) {
+            index += 1;
+            score += 1;
+            showText(index);
+            inputField.value = "";
+            something.textContent = "";
+            updateScore(score);
+        }
+        else if (inputField.value != textToType[index]) {
+            score -= 1;
+            inputField.value = "";
+            something.textContent = "Please type again!";
+            updateScore(score);
+        }
     }
-  } else {
-    inputField.value = "You cant type anymore";
-  }
-};
-
-const startCountdown = (timeleft) => {
-  let downloadTimer = setInterval(function () {
-    countdownField.textContent = timeleft;
-    if (timeleft <= 0) {
-      clearInterval(downloadTimer);
-      countdownField.textContent =
-        "Your time is over, you cant type anymore!";
-      youCanType = false;
+    else {
+        inputField.value = "You cant type anymore";
     }
-
-    timeleft -= 1;
-  }, 1000);
 };
-
-startGameBtn.addEventListener("click", () => {
-  startCountdown(timeleft);
-  showText(index);
-  youCanType = true;
+var startCountdown = function (timeleft) {
+    var downloadTimer = setInterval(function () {
+        countdownField.textContent = timeleft;
+        if (timeleft <= 0) {
+            clearInterval(downloadTimer);
+            countdownField.textContent =
+                "Your time is over, you cant type anymore!";
+            youCanType = false;
+        }
+        timeleft -= 1;
+    }, 1000);
+};
+startGameBtn.addEventListener("click", function () {
+    startCountdown(timeleft);
+    showText(index);
+    youCanType = true;
 });
-
-inputField.addEventListener("keyup", (e) => {
-  if (e.key == "Enter") {
-    checkIfMatch();
-  }
+inputField.addEventListener("keyup", function (e) {
+    if (e.key == "Enter") {
+        checkIfMatch();
+    }
 });
