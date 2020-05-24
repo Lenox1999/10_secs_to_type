ce15e350818cc65ed7129a74e3a3e65a1c944fa5
Niklas <p3er.guessf3ldt@gmail.com>
2020-05-24 12:40:10 +0200

    Update finished and errors are solved

--- src/scripts/vendor.js
+++
@@ -1,5 +0,0 @@
-var startGameBtn = document.querySelector("button");
-var inputField = document.querySelector("input");
-var showTextField = document.getElementById("show-text-to-type");
-var something = document.getElementById("show-if-right");
-var countdownField = document.getElementById("countdown");
